baa84b7c749617b4601a61a2050584cad40e0121
Trung Nguyen <trung150502@gmail.com>
2024-05-05 11:38:46 +0000

    update new

--- client/src/components/Products/FeaturedProduct.js
+++ client/src/components/Products/FeaturedProduct.js
@@ -15,7 +15,7 @@ const FeaturedProduct = () => {
         fetchProduct();
     }, [])
   return (
-    <div className='w-main'>
+    <div className='w-main pb-40'>
         <h3 className='text-[20px] font-semibold py-[15px] border-b-2 border-main'>FEATURED PRODUCT</h3>
         <div className='flex flex-wrap mt-[15px] mx-[-10px]'>
             {product?.map(el => (
