ea194cdbbc56d1e46094adff54cdceb7e2576c4a
panmer <149462342+panmer@users.noreply.github.com>
2024-09-06 17:46:40 +0700

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,93 @@
+import React from "react";
+import {BestSeller, Sidebar, Banner, DealDaily, FeaturedService, Service, CustomSliderService, HomeSearchBar} from '../../components' 
+import { useSelector } from "react-redux";
+import icons from '../../ultils/icon'
+import withBaseComponent from "hocs/withBaseComponent";
+import { createSearchParams } from "react-router-dom";
+import { toast } from 'react-toastify';
+
+const {MdArrowForwardIos} = icons
+const Home = ({navigate}) => {
+    const {newProducts} = useSelector(state => state.product)
+    const {categories} = useSelector(state => state.app)
+    const {isLogin, current} = useSelector(state => state.user)
+
+    const [objectSearchTerm, setObjectSearchTerm] = useState('');
+    const [locationSearchTerm, setLocationSearchTerm] = useState('');
+
+    const fetchServiceSearchResult = useCallback(() => {
+        const response = await apiGetServices();
+
+        if (response && response.services) {
+            console.log('+++++', response.services, '+++++')
+        }
+        else  {
+            toast.error('Failed To Get Services Data!')
+        }
+    }, []);
+
+    return (
+        <>
+            <div className="w-main flex mt-6">
+            <div className="flex flex-col gap-5 w-[25%] flex-auto">   
+                <Sidebar />
+                <DealDaily></DealDaily>
+            </div>
+            <div className="flex flex-col gap-5 pl-5 w-[75%] flex-auto ">
+                <Banner />
+                <HomeSearchBar
+                    setObjectSearchTerm={setObjectSearchTerm}
+                    setLocationSearchTerm={setLocationSearchTerm}
+                />
+                <BestSeller />
+            </div>
+        </div>
+        <div className="my-8">
+            <FeaturedService />
+        </div>
+        <div className="my-8 w-main ">
+            <h3 className='text-[20px] font-semibold py-[15px] border-b-2 border-main'>NEW ARRIVALS</h3>
+            <div className="mt-4 mx-[-10px]">
+                <CustomSliderService products={newProducts}/>
+            </div>
+        </div>
+        <div className="my-8 w-main ">
+        {/* <h3 className='text-[20px] font-semibold py-[15px] border-b-2 border-main'>HOT COLLECTIONS</h3>
+        <div className="flex flex-wrap gap-4 mt-4">
+                {categories?.filter(el => el.brand?.length>0)?.map(el =>(
+                    <div key={el._id} className="w-[396px]">
+                        <div className="border flex p-4 gap-4  min-h-[190px]">
+                            <img src={el?.image} alt="" className="w-[144px] flex-1 h-[129px] object-cover"/>
+                            <div className="flex-1 text-gray-700">
+                                <h4 className="uppercase font-semibold">{el?.title}</h4>
+                                <ul className="text-sm">
+                                    {el?.brand?.map(item => (
+                                        <span  
+                                        key={item} 
+                                        className="flex cursor-pointer hover:text-main hover:underline items-center gap-1 text-gray-500"
+                                        onClick={()=> navigate({
+                                            pathname: `/${el?.title?.toLowerCase()}`,
+                                            search: createSearchParams({
+                                                brand: item
+                                            }).toString()
+                                        })}
+                                        >
+                                            <MdArrowForwardIos size={14}/> 
+                                            <li>{item}</li>
+                                        </span>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div> */}
+        </div>
+        <div className="my-8 w-main">
+            <h3 className='text-[20px] font-semibold py-[15px] border-b-2 border-main'>BLOG POSTS</h3>
+
+        </div></>
+    )
+}
+
+export default withBaseComponent(Home)
---
+++ service.js
@@ -0,0 +1,376 @@
+const Service = require('../models/service')
+const User = require('../models/user')
+const asyncHandler = require("express-async-handler")
+const slugify = require('slugify')
+const makeSku = require('uniqid')
+
+
+const createService = asyncHandler(async(req, res)=>{
+    const {name, price, description, category, assigned_staff, hour, minute, provider_id} = req.body
+
+
+
+    const thumb = req.files?.thumb[0]?.path
+    const image = req.files?.images?.map(el => el.path)
+
+    if(!name || !price || !description || !assigned_staff || !category || !hour || !minute || !provider_id){
+        throw new Error("Missing input")
+    }
+    req.body.duration = +hour*60 + +minute
+    if(thumb) req.body.thumb = thumb
+    if(image) req.body.image = image
+    const newService = await Service.create(req.body)
+    return res.status(200).json({
+        success: newService ? true : false,
+        mes: newService ? 'Created successfully' : "Cannot create new service"
+    })
+})
+
+// get all staffs
+const getAllServicesByAdmin = asyncHandler(async (req, res) => {
+    const {_id} = req.user
+    const {provider_id} = await User.findById({_id}).select('provider_id')
+    const queries = { ...req.query };
+
+    // Loại bỏ các trường đặc biệt ra khỏi query
+    const excludeFields = ['limit', 'sort', 'page', 'fields'];
+    excludeFields.forEach((el) => delete queries[el]);
+
+    // Format lại các toán tử cho đúng cú pháp của mongoose
+    let queryString = JSON.stringify(queries);
+    queryString = queryString.replace(
+        /\b(gte|gt|lt|lte)\b/g,
+        (matchedEl) => `$${matchedEl}`
+    );
+
+    // chuyen tu chuoi json sang object
+    const formatedQueries = JSON.parse(queryString);
+
+    //Filtering
+    let categoryFinish = {}
+    if (queries?.name) formatedQueries.name = { $regex: queries.title, $options: 'i' };
+    if (queries?.category){
+        delete formatedQueries.category
+        const categoryArray = queries.category?.split(',')
+        const categoryQuery = categoryArray.map(el => ({
+            category: {$regex: el, $options: 'i' }
+        }))
+        categoryFinish = {$or: categoryQuery}
+    }
+
+    let queryFinish = {}
+    if(queries?.q){
+        delete formatedQueries.q
+        queryFinish = {
+            $or: [
+                {name: {$regex: queries.q, $options: 'i' }},
+                {category: {$regex: queries.q, $options: 'i' }},
+            ]
+        }
+    }
+    const qr = {...formatedQueries, ...queryFinish, ...categoryFinish, provider_id}
+    let queryCommand =  Service.find(qr).populate({
+        path: 'assigned_staff',
+        select: 'firstName lastName avatar',
+    })
+    try {
+        // sorting
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ')
+            queryCommand.sort(sortBy)
+        }
+
+        //filtering
+        if(req.query.fields){
+            const fields = req.query.fields.split(',').join(' ')
+            queryCommand.select(fields)
+        }
+
+        //pagination
+        //limit: so object lay ve 1 lan goi API
+        //skip: n, nghia la bo qua n cai dau tien
+        //+2 -> 2
+        //+dgfbcxx -> NaN
+        const page = +req.query.page || 1
+        const limit = +req.query.limit || process.env.LIMIT_PRODUCT
+        const skip = (page-1)*limit
+        queryCommand.skip(skip).limit(limit)
+
+
+        const services = await queryCommand
+        const counts = await Service.countDocuments(qr);
+        return res.status(200).json({
+            success: true,
+            counts: counts,
+            services: services,
+            });
+        
+    } catch (error) {
+        // Xử lý lỗi nếu có
+        return res.status(500).json({
+        success: false,
+        error: 'Cannot get services',
+        });
+    }
+})
+
+// get all staffs
+const deleteServiceByAdmin = asyncHandler(async (req, res) => {
+    const {sid} = req.params
+    const service = await Service.findByIdAndDelete(sid)
+    return res.status(200).json({
+        success: service ? true : false,
+        mes: service ? 'Deleted successfully' : "Cannot delete service"
+    })
+})
+
+const updateServiceByAdmin = asyncHandler(async(req, res)=>{
+    const {sid} = req.params
+
+    const files = req?.files
+    if(files?.thumb){
+        req.body.thumb = files?.thumb[0]?.path
+    }
+    if(files?.images){
+        req.body.image = files?.images?.map(el => el.path)
+    }
+    const service = await Service.findByIdAndUpdate(sid, req.body, {new: true})
+    return res.status(200).json({
+        success: service ? true : false,
+        mes: service ? 'Updated successfully' : "Cannot update service"
+    })
+})
+//update staff by admin
+// const updateStaffByAdmin = asyncHandler(async (req, res) => {
+//     const {staffId} = req.params
+
+//     if(!staffId || Object.keys(req.body).length === 0){
+//         throw new Error("Missing input")
+//     }
+//     else{
+//         const response = await Staff.findByIdAndUpdate(staffId, req.body, {new: true}).select('-refresh_token -password -role')
+//         return res.status(200).json({
+//             success: response ? true : false,
+//             mes: response ? `Staff with email ${response.email} updated successfully` : "Something went wrong"
+//         })
+//     }
+// })
+
+// delete staff by admin
+// const deleteStaffByAdmin = asyncHandler(async (req, res) => {
+//     const {staffId} = req.params
+//     if(!staffId){
+//         throw new Error("Missing input")
+//     }
+//     else{
+//         const response = await Staff.findByIdAndDelete(staffId)  
+//         return res.status(200).json({
+//             success: response ? true : false,
+//             mes: response ? `Staff with email ${response.email} deleted successfully` : "Something went wrong"
+//         })
+//     }
+// })
+
+
+// get all staffs
+const getAllServicesPublic = asyncHandler(async (req, res) => {
+    const queries = { ...req.query };
+
+    // Loại bỏ các trường đặc biệt ra khỏi query
+    const excludeFields = ['limit', 'sort', 'page', 'fields'];
+    excludeFields.forEach((el) => delete queries[el]);
+
+    // Format lại các toán tử cho đúng cú pháp của mongoose
+    let queryString = JSON.stringify(queries);
+    queryString = queryString.replace(
+        /\b(gte|gt|lt|lte)\b/g,
+        (matchedEl) => `$${matchedEl}`
+    );
+
+    // chuyen tu chuoi json sang object
+    const formatedQueries = JSON.parse(queryString);
+    //Filtering
+    let categoryFinish = {}
+    if (queries?.name) formatedQueries.name = { $regex: queries.title, $options: 'i' };
+    if (queries?.category){
+        delete formatedQueries.category
+        const categoryArray = queries.category?.split(',')
+        const categoryQuery = categoryArray.map(el => ({
+            category: {$regex: el, $options: 'i' }
+        }))
+        categoryFinish = {$or: categoryQuery}
+    }
+
+    let queryFinish = {}
+    if(queries?.q){
+        delete formatedQueries.q
+        queryFinish = {
+            $or: [
+                {name: {$regex: queries.q, $options: 'i' }},
+                {category: {$regex: queries.q, $options: 'i' }},
+            ]
+        }
+    }
+    const qr = {...formatedQueries, ...queryFinish, ...categoryFinish}
+    let queryCommand =  Service.find(qr).populate({
+        path: 'provider_id',
+        select: 'longitude latitude',
+    })
+    try {
+        // sorting
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ')
+            queryCommand.sort(sortBy)
+        }
+
+        if(req?.query?.current_location){
+            queryCommand.aggregate([
+                {
+                    $lookup: {
+                        from: 'Service_Provider',
+                        localField: 'provider_id',
+                        foreignField: '_id',
+                        as: 'provider'
+                    }
+                },
+                { $unwind: "$provider" },
+                {
+                    $addFields: {
+                        geoJSON: {
+                            type: "Point",
+                            coordinates: ["$provider.longitude", "$provider.latitude"]
+                        }
+                    }
+                },
+                {
+                    $sort: {
+                        geoJSON: -1
+                    }
+                }
+            ])
+        }
+
+        //filtering
+        if(req.query.fields){
+            const fields = req.query.fields.split(',').join(' ')
+            queryCommand.select(fields)
+        }
+
+        //pagination
+        //limit: so object lay ve 1 lan goi API
+        //skip: n, nghia la bo qua n cai dau tien
+        //+2 -> 2
+        //+dgfbcxx -> NaN
+        const page = +req.query.page || 1
+        const limit = +req.query.limit || process.env.LIMIT_PRODUCT
+        const skip = (page-1)*limit
+        queryCommand.skip(skip).limit(limit)
+
+
+        const services = await queryCommand
+        const counts = await Service.countDocuments(qr);
+        return res.status(200).json({
+            success: true,
+            counts: counts,
+            services: services,
+            });
+        
+    } catch (error) {
+        // Xử lý lỗi nếu có
+        return res.status(500).json({
+        success: false,
+        error: 'Cannot get services',
+        });
+    }
+})
+
+const getOneService = asyncHandler(async(req, res)=>{
+    const {sid} = req.params
+
+    const service = await Service.findById(sid).populate({
+        path: 'assigned_staff',
+        select: 'firstName lastName avatar mobile email work',
+    }).populate({
+        path: 'rating',
+        populate: {
+            path: 'postedBy',
+            select: 'firstName lastName avatar',
+        }
+    })
+
+
+    
+    return res.status(200).json({
+        success: service ? true : false,
+        service: service ? service : "Cannot find product"
+    })
+})
+
+const addVariantService = asyncHandler(async(req, res)=>{
+    const {sid} = req.params
+    const {name, price, hour, minute, description} = req.body
+    const thumb = req.files?.thumb[0]?.path
+    const image = req.files?.images?.map(el => el.path)
+    const duration = +hour*60 + +minute
+
+    if(!name || !price || !hour || !minute || !description){
+        throw new Error("Missing input")
+    }
+    const response = await Service.findByIdAndUpdate(sid, {$push: {variants: {name, price, thumb, image, duration, description, sku: makeSku().toUpperCase()}}},{new: true})
+    return res.status(200).json({
+        success: response? true : false,
+        mes: response? 'Add variant successfully' : "Cannot add variant"
+    })
+    
+})
+
+const ratingService = asyncHandler(async(req, res)=>{
+
+    const {_id} = req.user
+    const {star, comment, sid, updatedAt} = req.body
+
+    if(!star || !sid){
+        throw new Error("Missing input")
+    }
+    const ratingService = await Service.findById(sid)
+    
+    //alreadyRating tra ve element trong Rating neu co
+    const alreadyRating = ratingService?.rating?.find(e1 => e1.postedBy.toString() === _id)
+
+    if(alreadyRating){
+        await Service.updateOne(
+            {rating: {$elemMatch: alreadyRating}},
+            {$set: {"rating.$.star": star, "rating.$.comment": comment,  "rating.$.updatedAt": updatedAt}}
+            )
+    }
+    else{
+        await Service.findByIdAndUpdate(
+            sid,
+            {$push:{rating :{star, comment, postedBy: _id, updatedAt}}},
+            {new: true})
+    }
+
+    // Average rating
+    const updatedService = await Service.findById(sid)
+    const totalRatings = updatedService.rating.length
+    
+    // reduce: 2 doi so (callback + initial value)
+    const totalScores = updatedService.rating.reduce((sum,ele) => sum + (+ele.star),0)
+    updatedService.totalRatings = Math.round(totalScores/totalRatings)
+    await updatedService.save()
+
+    return res.status(200).json({
+        success: true,
+        updatedService
+    })
+})
+module.exports = {
+    createService,
+    getAllServicesByAdmin,
+    deleteServiceByAdmin,
+    updateServiceByAdmin,
+    getAllServicesPublic,
+    getOneService,
+    addVariantService,
+    ratingService
+}
