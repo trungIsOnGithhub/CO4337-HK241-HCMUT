2e58a339ba8dab2f5e5cf663622db688a616dfd7
trivien11vn <triviencoderr@gmail.com>
2024-09-24 01:14:50 +0700

    display email + avatar from signInWithOAuth

--- client/src/pages/user/Calendar.js
+++ client/src/pages/user/Calendar.js
@@ -4,11 +4,23 @@ import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import googleCalendar from '../../assets/google_calendar_icon.png'
+import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
+import { useNavigate } from 'react-router-dom'; // Thêm import này
+import path from 'ultils/path';
+import { Button } from 'components';
+import { FcGoogle } from "react-icons/fc";
+import { FaGoogle, FaSignOutAlt } from "react-icons/fa";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
 const Calendar = () => {
+  const navigate = useNavigate(); // Khởi tạo history
   const { current } = useSelector(state => state.user);
   const [calendar, setCalendar] = useState([]);
   const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const session = useSession();
+  const supabase = useSupabaseClient(); //Khởi tạo supabase Client
+  const { isLoading } = useSessionContext(); // Kiểm tra trạng thái loading
 
   useEffect(() => {
     const fetchCalendarByUserId = async () => {
@@ -60,12 +72,66 @@ const Calendar = () => {
     }
   };
 
-  const handleAddCalendarEvent = () => {
+  const handleAddCalendarEvent = async () => {
+    if (!session) {
+      await googleSignIn(); // Gọi hàm đăng nhập nếu chưa có session
+    } else {
+      // Gọi hàm tạo sự kiện lịch ở đây
+      await createCalendarEvent();
+    }
+  };
+
+  const googleSignIn = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        scopes: 'https://www.googleapis.com/auth/calendar',
+        redirectTo: 'http://localhost:3000/user/my_calendar' // Thêm redirectTo
+      }
+    });
+  
+    if (error) {
+      toast.error("Error logging in to Google provider with Supabase");
+    }
+  };
+
+  const createCalendarEvent = async () => {
     
+  };
+
+  const handleLogout = async() => {
+    await supabase.auth.signOut();
   }
+  
+  console.log(session?.user?.user_metadata?.avatar_url)
 
   return (
     <div className="max-w-6xl mx-auto p-4 bg-white shadow-lg rounded-lg flex flex-col overflow-y-hidden h-screen">
+      {
+        session && 
+        <div className='flex justify-between items-center'>
+          <div className='flex items-center gap-4 p-4 bg-gray-100 shadow-md rounded-lg'>
+            <h2 className='text-xl text-gray-700 font-semibold hover:text-gray-900 transition-colors duration-200'>
+                Hey: {session?.user?.email}
+            </h2>
+            {
+                session?.user?.user_metadata?.picture &&  
+                <img 
+                    src={session?.user?.user_metadata?.picture} 
+                    className='w-12 h-12 rounded-full border border-gray-300 shadow-sm transition-transform transform hover:scale-105' 
+                />
+            }
+          </div>
+          <Button
+            handleOnclick={handleLogout} 
+            style={'flex items-center px-4 py-2 text-white bg-red-600 rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 transition-all duration-300 ease-in-out'}
+          >
+            <FaGoogle className="mr-2" />
+            <span className="mr-2">Logout</span>
+            <FaSignOutAlt />
+          </Button>
+        </div>
+      }
       <div className="flex justify-between items-center mb-4 h-[10%]">
         <button onClick={() => changeMonth('prev')}><FaChevronLeft /></button>
         <h2 className="font-semibold">{`${new Date(currentYear, currentMonth).toLocaleString('default', { month: 'long' })} ${currentYear}`}</h2>
