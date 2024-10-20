27429811a89cdbd2c37a18ad0d87609071bbfd1c
trung <trung>
2024-10-20 14:43:40 +0700

    add config note

--- server/services/constant.js
+++ server/services/constant.js
@@ -3,3 +3,9 @@ module.exports = {
     BLOGS: "datnhk241blogs",
     HEALTH_STATUS_TIMEOUT: "2s"
 }
+/*
+DISABLE ELASTIC SECURITY:
+xpack.security.enabled: false
+xpack.security.transport.ssl.enabled: false
+xpack.security.http.ssl.enabled: false
+*/
