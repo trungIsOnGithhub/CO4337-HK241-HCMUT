171ca60360cd6118aa0e4720d25b7a75a58f44af
trung <trung@gmail.com>
2024-10-23 22:01:49 +0700

    update db connect for inte test

--- server/tests/integration/common.js
+++ server/tests/integration/common.js
@@ -1,10 +1,28 @@
 // https://www.chaijs.com/plugins/chai-http/
 require('dotenv').config()
 const expressApp = require('../../app');
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 8888;
 
+const testDBConnect = async function() {
+    const MONGODB_URI = "";
+    try{
+        const conn = await mongoose.connect()
+        if(conn.connection.readyState === 1){
+
+        }
+        else{
+
+        }
+    } catch(error){
+
+        throw new Error(error)
+    }
+}
+
 module.exports = {
+    testDBConnect,
     expressApp,
     TEST_BASE_URL: `http://127.0.0.1:${PORT}`
 }
