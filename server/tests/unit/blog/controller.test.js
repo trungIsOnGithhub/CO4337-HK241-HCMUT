4e95f15a29b55eb0f9641430335d93a1f077ec43
panmer <149462342+panmer@users.noreply.github.com>
2024-10-26 07:48:16 +0700

    update generic test

--- server/tests/unit/blog/controller.test.js
+++ server/tests/unit/blog/controller.test.js
@@ -26,14 +26,9 @@ async function genericControllerThrowErrorTest(mock, match, controllerFunc) {
 
   chai.expect(result).to.not.be.null;
   chai.assert.fail("No Error", "Error Thrown", "Expected throw Error but not");
-
-  return {
-    ok: true, // Test Performed OK
-    msg: "Behavior NOT matched"
-  }; 
 }
 
-async function genericControllerPostSuccessTest(mock, match, controllerFunc) {
+async function genericControllerSuccessTest(mock, match, controllerFunc) {
   const resp = new TestResponse();
 
   let result = null;
@@ -41,19 +36,49 @@ async function genericControllerPostSuccessTest(mock, match, controllerFunc) {
     result = await controllerFunc(mock, resp);
   }
   catch(err) {
-    console.log("00000===========000000");
-    return {
-      ok: true, // Test Performed OK
-      msg: "Behavior matched"
-    };
+    chai.assert.fail("Error Thrown", "No Error", "Throw Error while not Expected");
   }
 
   chai.expect(result).to.not.be.null;
-  chai.assert.fail("No Error", "Error Thrown", "Expected throw Error but not");
+  chai.expect(result.success).to.be.true;
+
+  const matchKeys = Object.keys(match);
+  for (const key of matchKeys) {
+      // console.log(key, "======>", match[key] + "---||---" + typeof(match[key]));
+
+      chai.expect(result[key]).to.not.be.null;
+      chai.expect(typeof result[key])
+          .to.equal(typeof match[key]);
+
+      if (typeof(match[key]) === 'object') {
+        const innerResult = result[key];
+        const innerMatch = match[key];
+        const matchKeys = Object.keys(innerMatch);
+
+        for (const innerKey of matchKeys) {
+          // console.log(innerKey, ":::::::>",  innerResult[innerKey] + "+++||+++" + typeof(innerMatch[innerKey]));
+
+          chai.expect(innerResult[innerKey]).to.not.be.null;
+          chai.expect(
+            typeof innerResult[innerKey]
+          ).to.equal(
+            typeof innerMatch[innerKey]
+          );
+
+          if (innerMatch.length) {
+            chai.expect(innerResult.length).to.equal(innerMatch.length);
+          }
+        }
+      }
+
+      if (match.length) {
+        chai.expect(result.length).to.equal(match.length);
+      }
+  }
 
   return {
     ok: true, // Test Performed OK
-    msg: "Behavior NOT matched"
+    msg: "Behavior matched"
   }; 
 }
 
@@ -66,7 +91,7 @@ describe('Test Sample 1 - Blog API', async function() {
     it('BL2-1_POST_/api/blog/_200_CreateSuccess', async function() {
       const { mock, match } = currentMockUnitTestData[this.test.title];
 
-      await genericControllerThrowErrorTest(
+      await genericControllerSuccessTest(
         mock, match,
         blogsAPIControllers.createNewBlogPost
       );
--- server/tests/unit/blog/mock.test.js
+++ server/tests/unit/blog/mock.test.js
@@ -2,12 +2,22 @@ module.exports = {
     'Test Sample 1 - Blog API': {
         'BL2-1_POST_/api/blog/_200_CreateSuccess': {
             mock: {
-                title: "",
-                content: null
+                body: {
+                    title: "sample",
+                    content: "sample"
+                }
             },
             match: {
-                success: false,
-                mes: "Missing input"
+                createBlog: {
+                    _id: "66377327edf989f1ae865513",
+                    title: "Sample Title",
+                    content: "An interesting blog...",
+                    category: "Sample category",
+                    numberView: 9999,
+                    likes:[],
+                    dislikes:[],
+                    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.123rf.com%2Fphoto_133391293_creative-blogging-sketch-on-white-brick-wall-background-blog-and-media-concept-3d-rendering.html&psig=AOvVaw0nd0jBQJauaxJrqQ8TtS9z&ust=1699960308658000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLia9eTrwIIDFQAAAAAdAAAAABAI"
+                }
             }
         },
         'BL2-2_POST_/api/blog/_400_MissingInput': {
